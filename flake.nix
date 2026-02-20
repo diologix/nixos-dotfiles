a9e909ca2ff3098d3d2aa110b9a63e920603d57d
diologix <62773168+diologix@users.noreply.github.com>
2026-02-20 11:27:58 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,67 @@
 {
- description="Senior Laptop Lanzaboote";
- inputs={
-  nixpkgs.url="github:NixOS/nixpkgs/nixos-25.11";
+  description = "NIXG + NIXP NixOS with Hyprland, TPM/LUKS/Lanzaboote, Home Manager";
 
-  lanzaboote.url="github:nix-community/lanzaboote";
-  lanzaboote.inputs.nixpkgs.follows="nixpkgs";
- };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # adjust as needed
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
- outputs={self,nixpkgs,lanzaboote,...}:
+    lanzaboote.url = "github:nix-community/lanzaboote";
+    lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
 
- let
-  system="x86_64-linux";
- in{
+    noctalia.url = "github:noctalia-dev/noctalia-shell";
+    noctalia.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  nixosConfigurations.nixos-laptop=
-   nixpkgs.lib.nixosSystem{
-    inherit system;
-    modules=[
-     ./configuration.nix
-     lanzaboote.nixosModules.lanzaboote
-    ];
+  outputs = { self, nixpkgs, home-manager, lanzaboote, noctalia, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgsFor = system: import nixpkgs { inherit system; config.allowUnfree = true; };
+  in {
+    nixosConfigurations = {
+      NIXG = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/common/base.nix
+          ./hosts/common/users.nix
+          ./hosts/common/desktop.nix
+          ./hosts/common/virtualisation.nix
+          ./hosts/common/lanzaboote.nix
 
-   };
+          ./hosts/NIXG/configuration.nix
 
- };
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hr = import ./home/hr/home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
+
+      NIXP = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/common/base.nix
+          ./hosts/common/users.nix
+          ./hosts/common/desktop.nix
+          ./hosts/common/virtualisation.nix
+          ./hosts/common/lanzaboote.nix
+
+          ./hosts/NIXP/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hr = import ./home/hr/home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
+    };
+  };
 }
