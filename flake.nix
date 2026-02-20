d37b83edf94cb40fbfd995f118152635f800ba6e
diologix <62773168+diologix@users.noreply.github.com>
2026-02-20 11:10:35 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,27 @@
 {
- description="Senior Laptop";
-
+ description="Senior Laptop Lanzaboote";
  inputs={
-  nixpkgs.url="github:NixOS/nixpkgs/nixos-25.11";
+  nixpkgs.url="github:NixOS/nixpkgs/nixos-24.11";
+
+  lanzaboote.url="github:nix-community/lanzaboote";
+  lanzaboote.inputs.nixpkgs.follows="nixpkgs";
  };
 
- outputs={self,nixpkgs,...}:
+ outputs={self,nixpkgs,lanzaboote,...}:
+
  let
   system="x86_64-linux";
  in{
+
   nixosConfigurations.nixos-laptop=
    nixpkgs.lib.nixosSystem{
     inherit system;
     modules=[
      ./configuration.nix
+     lanzaboote.nixosModules.lanzaboote
     ];
+
    };
+
  };
 }
