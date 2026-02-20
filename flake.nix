a8d4d238f493af7ea45a806b5be7049dfde8d0cb
diologix <62773168+diologix@users.noreply.github.com>
2026-02-20 10:12:48 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+ description="Senior Laptop";
+
+ inputs={
+  nixpkgs.url="github:NixOS/nixpkgs/nixos-25.11";
+ };
+
+ outputs={self,nixpkgs,...}:
+ let
+  system="x86_64-linux";
+ in{
+  nixosConfigurations.nixos-laptop=
+   nixpkgs.lib.nixosSystem{
+    inherit system;
+    modules=[
+     ./configuration.nix
+    ];
+   };
+ };
+}
